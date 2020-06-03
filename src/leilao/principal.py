73c01e8a4934da49a35a3073f064a16c3f3ed069
jedsonBrito <jedsoncid@gmail.com>
2020-06-03 11:01:32 -0300

    Incluindo a classe de avaliar o maior e menor lance

--- src/leilao/principal.py
+++ src/leilao/principal.py
@@ -1,16 +1,24 @@
-from src.leilao.dominio import Usuario,Lance,Leilao
+from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 jedi = Usuario("Jedi")
 sith = Usuario("Sith")
+
 
 lance_do_sith = Lance(sith,100)
 lance_do_jedi = Lance(jedi,150)
 
 leilao_corruscant = Leilao("Celular")
 
-leilao_corruscant.lances.append(lance_do_sith)
 leilao_corruscant.lances.append(lance_do_jedi)
+leilao_corruscant.lances.append(lance_do_sith)
+
 
 
 for lance in leilao_corruscant.lances:
     print(f'O Usuario {lance.usuario.nome} deu um lance de {lance.valor}')
+
+avaliacao = Avaliador()
+
+avaliacao.avalia(leilao_corruscant)
+
+print(f'o maior lance foi {avaliacao.maior_lance} o menor lance foi {avaliacao.menor_lance}')
